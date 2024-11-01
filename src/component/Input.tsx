da9336de31c5c1d313f3ed01fb5a11e86d8ed17b
Gary Fehr <gfehr@key2act.com>
2024-11-01 07:56:37 -0500

    Lesson 51

---
+++ src/component/Input.tsx
@@ -0,0 +1,12 @@
+type InputProps = {
+    label: string;
+    id: string;
+}
+export default function Input({label, id}: InputProps) {
+    return (
+        <p>
+            <label htmlFor={id}>{label}</label>
+            <input id={id} />
+        </p>
+    )
+}
