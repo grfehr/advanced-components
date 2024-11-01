2805400447bd798d0e2d5006f60dea0dd39d0380
Gary Fehr <gfehr@key2act.com>
2024-11-01 11:03:20 -0500

    Lesson 60

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import Form from "./component/Form.tsx";
 import Button from './component/Button.tsx';
 
 function App() {
+    function handleSave(data: unknown) {
+        const extractedData = data as {name: string; age: string};
+        console.log(extractedData);
+    };
+
     return (
         <main>
-            <Form>
+            <Form onSave={handleSave}>
                 <Input label={"Name"} type={"text"} id={"name"} />
                 <Input label={"Age"} type={"number"} id={"age"} />
                 <p><Button>Save</Button></p>
