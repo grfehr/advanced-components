58686bf37bb42e2f7f405eccd4429d2246202419
Gary Fehr <gfehr@key2act.com>
2024-11-01 08:42:05 -0500

    Lesson 54

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,17 +1,21 @@
 import {ComponentPropsWithoutRef} from "react";
 
-type ButtonProps = {
-    el: 'button'
-} & ComponentPropsWithoutRef<'button'>;
+type ButtonProps = ComponentPropsWithoutRef<'button'> & {
+    href?: never;
+};
 
-type AnchorProps = {
-    el: 'anchor'
-} & ComponentPropsWithoutRef<'a'>;
+type AnchorProps = ComponentPropsWithoutRef<'a'> & {
+    href?: string;
+};
+
+function isAnchorProp(props: ButtonProps | AnchorProps): props is AnchorProps {
+    return 'href' in props;
+}
 
 export default function Button(props: ButtonProps | AnchorProps) {
     // const { el, ...otherProps} = props;
-    if (props.el === 'anchor') {
-        return <a className={"buton"} {...props}></a>
+    if (isAnchorProp(props)) {
+        return <a className={"button"} {...props}></a>
     }
 
     return <button className={"button"} {...props}></button>
